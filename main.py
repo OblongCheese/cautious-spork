504e3933c1fa9872a03988088d5f9408df5b3613
OblongCheese <oblongcheese@gmail.com>
2018-07-09 16:36:46 +1000

    Initial commit
    Signed-off-by: Owen Stickley <owen@owened.net>

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# testing 1, 2, 3...
+# ----------------------------------------------------------------------------
+# I want to build static HTML pages that have information about household
+# expenditure. I want to create an index page that links to each month.
+#
+# * one month worth of transactions = 1 static HTML page
+# * one index page to link to all the months and provide an FY YTD overview
+# * as each FY passes, generate the FY summary page for each
+# * categorize income and expenses based on regex / predefined lists
+# -----------------------------------------------------------------------------
